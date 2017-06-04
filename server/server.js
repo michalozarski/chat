290c8423f1722592182d342611105d0fbe8ba707
Michal Ozarski <Michal Ozarski>
2017-06-04 20:57:00 +0200

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () =>  {
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  console.log(`users connected: ${io.engine.clientsCount}`);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    console.log(`users connected: ${io.engine.clientsCount}`);
+  })
+});
+
+server.listen(port, () =>  {
   console.log(`server running on port ${port}`);
 });
