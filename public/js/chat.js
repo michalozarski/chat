816ec2b7c1d457d0bcf67f067e729ce98e3c9679
Michal Ozarski <Michal Ozarski>
2017-07-24 22:18:42 +0200

    messages sent to same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -29,7 +29,7 @@
   socket.on('updateUserList', function (users) {
     var ol = $('<ol></ol>');
     users.forEach(function (user) {
-      ol.append($('<li></li>').text(user));
+      ol.append($('<li></li>').text(user))    ;
     });
 
     $('#users').html(ol);
--- server/server.js
+++ server/server.js
@@ -40,8 +40,14 @@ io.on('connection', (socket) => {
   })
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+
+    if(user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
+
+
     callback({
       status: 'statusHere',
       err: 'this is err'
@@ -49,7 +55,10 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    if(user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));      
+    }
   });
 
   socket.on('disconnect', () => {
